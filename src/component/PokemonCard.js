047ffb7561b40407c30276694b8e22ab4d48f0d0
C110156253 <C110156253@nkust.edu.tw>
2024-09-12 14:56:35 +0800

    modified:   src/component/PokemonCard.js 	modified:   src/css/App.css

--- src/component/PokemonCard.js
+++ src/component/PokemonCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../css/App.css';
 
 function PokemonCard({ pokemon, onClick }) {
   return (
-    <div onClick={onClick} style={{ border: '1px solid black', padding: '10px', margin: '5px' }}>
-      <img src={pokemon.sprite} alt={pokemon.name} />
+    <div onClick={onClick} className='card'>
+      <img  src={pokemon.sprite} alt={pokemon.name} />
       <h2>{pokemon.name}</h2>
       <p>Age: {pokemon.age}</p>
       <p>Health: {pokemon.health}</p>
