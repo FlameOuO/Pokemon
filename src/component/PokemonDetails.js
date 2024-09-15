ab2211253fc95c84a54a4898b947052cfc57cbff
MarionLove <adventchildalisha@hotmail.com>
2024-09-15 15:23:05 +0800

    fix: audio source, promise, CSS

--- src/component/PokemonDetails.js
+++ src/component/PokemonDetails.js
@@ -1,20 +1,28 @@
+import AudioPlayer from './AudioPlayer';
 function PokemonDetails({ pokemon, onFeed, onWater, onPlay }) {
+	return (
+		<>
+			<div className="details">
+				<h2>{pokemon.name} Details</h2>
+				<img src={pokemon.sprite} alt={pokemon.name} />
+				<p>Age: {pokemon.age}</p>
+				<p>Health: {pokemon.health}</p>
+				<p>Hunger: {pokemon.hunger}</p>
+				<p>
+					Mood: {pokemon.mood} {pokemon.mood > 50 ? '😊' : '😞'}
+				</p>
+				<div>
+					<button onClick={onFeed}>餵食</button>
+					<button onClick={onWater}>喝水</button>
+					<button onClick={onPlay}>玩耍</button>
+				</div>
+				<div className="audioContainer">
+					<p>當前寶可夢: {pokemon.audioName}</p>
 
-  return (
-    <div className='details'>
-      <h2>{pokemon.name} Details</h2>
-      <img src={pokemon.sprite} alt={pokemon.name} />
-      <p>Age: {pokemon.age}</p>
-      <p>Health: {pokemon.health}</p>
-      <p>Hunger: {pokemon.hunger}</p>
-      <p>Mood: {pokemon.mood} {pokemon.mood >50 ?  '😊' : '😞'}</p>
-      <div>
-      <button onClick={onFeed}>餵食</button>
-      <button onClick={onWater}>喝水</button>
-      <button onClick={onPlay}>玩耍</button>
-      </div>
-
-    </div>
-  );
+					<AudioPlayer audioName={pokemon.audioName} />
+				</div>
+			</div>
+		</>
+	);
 }
 export default PokemonDetails;
